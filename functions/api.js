102a05e3a1a862339804afaf7831156e72825d3c
hrithikscube <hrithik@scube.me>
2024-05-20 12:06:19 +0530

    updated endpoints

--- functions/api.js
+++ functions/api.js
@@ -13,5 +13,9 @@ router.get('/', (req, res) => {
   res.send('Edge function running successfully');
 });
 
+router.get('/todos', (req, res) => {
+  res.send(todos).status(200);
+});
+
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
