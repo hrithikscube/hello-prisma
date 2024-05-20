f9ec390b18c108d19242ba3e0ed907c082b0bc81
hrithikscube <hrithik@scube.me>
2024-05-20 12:11:09 +0530

    updated post method

--- functions/api.js
+++ functions/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
+router.use(express.json())
 
 let todos = [
   {
@@ -15,6 +16,14 @@ router.get('/', (req, res) => {
 
 router.get('/todos', (req, res) => {
   res.send(todos).status(200);
+});
+
+router.post('/todos', (req, res) => {
+  let { title } = req.body
+  todos.push({
+    title: title
+  })
+  res.send('todo added successfully').status(200);
 });
 
 app.use('/.netlify/functions/api', router);
