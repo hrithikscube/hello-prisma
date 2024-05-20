fecac1782cd9721da2ea4b65b99b8a2e6420040e
hrithikscube <hrithik@scube.me>
2024-05-20 12:01:33 +0530

    updated edge function

--- functions/api.js
+++ functions/api.js
@@ -3,42 +3,21 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
-let records = [];
-
+let todos = [
+  {
+    title: 'This is a sample todo'
+  }
+];
 
 router.get('/', (req, res) => {
-  res.send('App is running..');
+  res.send('Edge function running successfully');
 });
 
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
+router.get('/todos', (req, res) => {
+  res.send({
+    data: todos
+  }).status(200)
+})
 
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Aayush',
-    },
-    {
-      id: '002',
-      name: 'rohit',
-    },
-    {
-      id: '003',
-      name: 'Mohit',
-    },
-  ]);
-});
-
-app.use('/.netlify/functions/api', router);
+app.use('/api', router);
 module.exports.handler = serverless(app);
